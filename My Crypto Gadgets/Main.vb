886b420331ee2050ac8bcd9081b1440a64454562
Rômulo Meirelles <52598055+Romulo-Meirelles@users.noreply.github.com>
2021-08-20 20:39:43 -0300

    Update Main.vb

--- My Crypto Gadgets/Main.vb
+++ My Crypto Gadgets/Main.vb
@@ -13,7 +13,23 @@ Module Main
             If parametros.Length > 1 Then
                 For Each par In parametros
                     If par = "-hide" Then
+                        If My.Settings.BTC_Ativate = False And _
+                            My.Settings.LTC_Ativate = False And _
+                            My.Settings.ETH_Ativate = False And _
+                            My.Settings.ADA_Ativate = False And _
+                            My.Settings.BCH_Ativate = False And _
+                            My.Settings.DOGE_Ativate = False And _
+                            My.Settings.NANO_Ativate = False And _
+                            My.Settings.XMR_Ativate = False And _
+                            My.Settings.XRP_Ativate = False And _
+                            My.Settings.My_IP_Activate = False Then
 
+                            Application.EnableVisualStyles()
+                            Application.SetCompatibleTextRenderingDefault(False)
+                            Application.Run(Config)
+                            MUTEX.ReleaseMutex()
+                        End If
+                    
                         Application.EnableVisualStyles()
                         Application.SetCompatibleTextRenderingDefault(False)
                         Config.WindowState = FormWindowState.Minimized
